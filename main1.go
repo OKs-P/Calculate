1d04edac16acdd668cbae5c7fe24feb51b56c6b6
OKs-P <oksanapankina21@gmail.com>
2024-07-11 22:21:07 +0300

    Calculater version1

---
+++ main1.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var mapRomNum = map[string]int{
+	"I":    1,
+	"II":   2,
+	"III":  3,
+	"IV":   4,
+	"V":    5,
+	"VI":   6,
+	"VII":  7,
+	"VIII": 8,
+	"IX":   9,
+	"X":    10,
+}
+
+var mapArabNum = map[int]string{
+	100: "C",
+	90:  "XC",
+	50:  "L",
+	40:  "XL",
+	10:  "X",
+	9:   "IX",
+	5:   "V",
+	4:   "IV",
+	1:   "I",
+}
+
+var mySort = [9]int{
+	100,
+	90,
+	50,
+	40,
+	10,
+	9,
+	5,
+	4,
+	1,
+}
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Введите значение: ")
+	_ = scanner.Scan()
+	example := scanner.Text()
+
+	fmt.Println(calculate(example))
+}
+
+func calculate(example string) string {
+
+	text := strings.Split(strings.TrimSpace(strings.ToUpper(example)), "")
+	operator := ""
+
+	for _, val := range text {
+		if val == "+" || val == "-" || val == "*" || val == "/" {
+			operator += string(val)
+			break
+		}
+	}
+
+	strNum := strings.Split(strings.TrimSpace(strings.ToUpper(example)), operator)
+
+	if len(strNum) < 2 {
+		panic("Выдача паники, так как строка не является математической операцией.")
+	}
+	if len(strNum) > 2 {
+		panic("Выдача паники, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+	}
+
+	firstNum, err1 := strconv.Atoi(strings.TrimSpace(strNum[0]))
+	sacondNum, err2 := strconv.Atoi(strings.TrimSpace(strNum[1]))
+	if err1 == nil && err2 == nil {
+		if firstNum > 10 || sacondNum > 10 {
+			panic(fmt.Sprint("Калькулятор работает только с числами до 10"))
+		}
+		return strconv.Itoa(count(firstNum, sacondNum, operator))
+
+	}
+	if err1 != nil && err2 != nil {
+		firstNum = romToInt(strings.TrimSpace(strNum[0]))
+		sacondNum = romToInt(strings.TrimSpace(strNum[1]))
+		if firstNum > 10 || sacondNum > 10 {
+			panic(fmt.Sprint("Калькулятор работает только с числами до X"))
+		}
+		return arabToRom(count(firstNum, sacondNum, operator))
+	}
+	if err1 != nil || err2 != nil {
+
+		panic(fmt.Sprint("Калькулятор работает только с числами одного типа"))
+	}
+
+	panic(fmt.Sprint("Некоректное выражение"))
+}
+
+func romToInt(arg string) int {
+
+	_, isPresent := mapRomNum[arg]
+	if isPresent {
+		return mapRomNum[arg]
+	}
+	return 100
+
+}
+func count(firstNum, sacondNum int, operator string) int {
+
+	result := 0
+	switch operator {
+	case "+":
+		result = firstNum + sacondNum
+	case "-":
+		result = firstNum - sacondNum
+	case "*":
+		result = firstNum * sacondNum
+	case "/":
+		result = firstNum / sacondNum
+
+	}
+
+	return result
+
+}
+func arabToRom(num int) string {
+	if num < 1 {
+		panic("меньше 0")
+	}
+	var res string = ""
+	var n int = num
+	for n > 0 {
+		for _, val := range mySort {
+			if val <= n {
+				res += mapArabNum[val]
+				n -= val
+				break
+			}
+
+		}
+	}
+	return res
+
+}
